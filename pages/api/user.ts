a421dbfa2bddce856b2954988db629d70049118d
bludnic <victorcpla@gmail.com>
2021-09-09 16:38:50 +0100

    fix(CI): replace `process.env.FIREBASE_PRIVATE_KEY` new lines

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -8,7 +8,9 @@ if (admin.apps.length === 0) {
     admin.initializeApp({
         credential: admin.credential.cert({
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY
+                ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+                : '',
             projectId: firebaseConfig.projectId,
         }),
     });
